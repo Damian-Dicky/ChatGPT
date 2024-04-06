3fb23f3863db8eacd155c67c1c6aa84e0a3a143a
Damian-Dicky <ogskippy23@gmail.com>
2024-04-06 13:29:35 +0800

    file

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,12 +1,12 @@
 import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
 import OpenAI from "openai";
-// const client = new OpenAIClient(
-//     process.env.AZURE_ENDPOINT!,
-//     new AzureKeyCredential(process.env.AZURE_APR_KEY!)
-// );
-// export default client;
-
-const client = new OpenAI({
-    apiKey: process.env.AZURE_API_KEY!, // This is the default and can be omitted
-});
+const client = new OpenAIClient(
+    process.env.AZURE_ENDPOINT!,
+    new AzureKeyCredential(process.env.AZURE_APR_KEY!)
+);
 export default client;
+
+// const client = new OpenAI({
+//     apiKey: process.env.AZURE_API_KEY!, // This is the default and can be omitted
+// });
+// export default client;
